5079b78cb23a37890ba1b5c6535a46681fe3f95d
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-12-17 14:45:35 +0000

    feat: neo-tree expanders

--- config/ui/tree/default.nix
+++ config/ui/tree/default.nix
@@ -5,8 +5,26 @@
       enable = true;
       enableGitStatus = true;
       enableRefreshOnWrite = true;
+
+      defaultComponentConfigs = {
+        indent = {
+          withExpanders = true;
+          expanderCollapsed = "";
+          expanderExpanded = "";
+          expanderHighlight = "NeoTreeExpander";
+        };
+      };
+
       window = {
         position = "float";
+      };
+
+      filesystem = {
+        filteredItems = {
+          hideDotfiles = false;
+          alwaysShow = [ ".gitignore" "flake.nix" ];
+          hideByPattern = [ "*/.git" ];
+        };
       };
     };
   };
